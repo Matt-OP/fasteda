cbec98f01095f27b2f9ec6b57f1db644a0b3f495
Matt OP <105291392+Matt-OP@users.noreply.github.com>
2023-01-24 15:31:24 -0500

    Updated to version 1.0.1

--- fasteda/__init__.py
+++ fasteda/__init__.py
@@ -1,11 +1,14 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-import matplotlib.pyplot as plt #graphing
-import seaborn as sns #graphing
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
+import seaborn as sns
+from scipy.stats import skew, kurtosis
 import missingno as msno
 from colorama import Fore, Back, Style
 
 plt.style.use("fivethirtyeight")
+pd.set_option("display.max_columns", 500)
 
 
 def fast_eda(df, target=None, correlation=True, pairplot=True, hist_box_plot=True, countplot=True):
@@ -31,7 +34,7 @@ def fast_eda(df, target=None, correlation=True, pairplot=True, hist_box_plot=Tru
         df0 = df[null_cols]
 
         display(df0.isna().sum().to_frame().style \
-                .set_properties(**{"background-color": "#000000", "color": "#ff0000"}))
+                .set_properties(**{"background-color": "#000000", "color": "#ff0000", "font-weight": "bold"}))
         print("-" * 100)
         
         # MSNO Matrix
@@ -40,7 +43,7 @@ def fast_eda(df, target=None, correlation=True, pairplot=True, hist_box_plot=Tru
             if len(null_cols) > 0:
                 print(f"{Fore.RED}{Style.BRIGHT}MSNO Matrix:{Style.RESET_ALL}")
                 print()
-                ax = msno.matrix(df, figsize = (12, 8))
+                ax = msno.matrix(df, color = (0, 0.5, 0), figsize = (12, 8))
                 plt.show()
                 print("-" * 100)
         
@@ -63,13 +66,33 @@ def fast_eda(df, target=None, correlation=True, pairplot=True, hist_box_plot=Tru
         
         print(f"{Fore.BLUE}{Style.BRIGHT}Describe DataFrame:{Style.RESET_ALL}")
         print()
+        
+        def color_negative_red(value):
+            if value < 0: color = "#ff0000"
+            elif value > 0: color = "#00ff00"
+            else: color = "#FFFFFF"
+            return "color: %s" % color
+        
+        skew_ = df._get_numeric_data().dropna().apply(lambda x: skew(x)).to_frame(name = "skewness")
+        kurt_ = df._get_numeric_data().dropna().apply(lambda x: kurtosis(x)).to_frame(name = "kurtosis")
+        skew_kurt = pd.concat([skew_, kurt_], axis = 1)
 
-        if df.columns.nunique() < 12:
-            display(df.describe().style.background_gradient(cmap = "Spectral"))
-            print("-" * 100)
-        else:
-            display(df.describe().T.style.background_gradient(cmap = "Spectral"))
-            print("-" * 100)
+        desc_df = df.describe().T
+        
+        full_info = pd.concat([desc_df, skew_kurt], ignore_index = True, axis = 1)
+        full_info.columns = list(desc_df.columns) + list(skew_kurt.columns)
+        full_info.insert(loc = 2, column = "median", value = df.median(skipna = True, numeric_only = True))
+        
+        full_info.iloc[:,:-2] = full_info.iloc[:,:-2].applymap(lambda x: format(x, '.3f') \
+                                         .rstrip('0').rstrip('.') if isinstance(x, (int, float)) else x)
+        
+        info_cols = ["skewness", "kurtosis"]
+        
+        display(full_info.style.background_gradient(cmap = "Spectral", subset = full_info.columns[:-2])
+                         .applymap(color_negative_red, subset = info_cols)
+                         .set_properties(**{"background-color": "#000000", "font-weight": "bold"}, subset = info_cols)
+                         .set_properties(**{"font-weight": "bold"}, subset = full_info.columns[:-2]))
+        print("-" * 100)
         
         # categorical variables
         
@@ -136,19 +159,75 @@ def fast_eda(df, target=None, correlation=True, pairplot=True, hist_box_plot=Tru
             print(f"{Fore.YELLOW}{Style.BRIGHT}Histogram(s) & Boxplot(s):{Style.RESET_ALL}")
             print()
             
-            for index, col in enumerate(num_cols):
+            if not target:
+            
+                for idx, col in enumerate(num_cols):
+
+                    fig, (ax1, ax2) = plt.subplots(1, 2, figsize = (14, 6))
+
+                    sns.histplot(df, x = df[col], kde = True,
+                                 color = sns.color_palette("hls", len(num_cols))[idx], ax = ax1)
+
+                    sns.boxplot(x = df[col], width = 0.4, linewidth = 3, fliersize = 2.5,
+                                color = sns.color_palette("hls", len(num_cols))[idx], ax = ax2)
+
+                    fig.suptitle(f"Histogram and Boxplot of {col}", size = 20, y = 1.02)
+                    plt.show()
+                    
+            elif target and df[target].nunique() == 2:
                 
-                fig, (ax1, ax2) = plt.subplots(1, 2, figsize = (14, 6))
+                for idx, col in enumerate(num_cols):
 
-                sns.histplot(df, x = df[col], kde = True, hue = target,
-                             color = sns.color_palette("hls", df.shape[1])[index], ax = ax1)
+                    fig, (ax1, ax2) = plt.subplots(1, 2, figsize = (14, 6))
 
-                sns.boxplot(x = df[col], width = 0.4, linewidth = 4, fliersize = 2.5,
-                            color = sns.color_palette("hls", df.shape[1])[index], ax = ax2)
+                    sns.histplot(df, x = df[col], kde = True, hue = target,
+                                 color = sns.color_palette("hls", len(num_cols))[idx], ax = ax1)
+                    
+                    box_dict =  {"boxprops":     dict(color = "#000000", linewidth = 2),
+                                 "capprops":     dict(color = "#000000", linewidth = 1.5),
+                                 "medianprops":  dict(color = "#000000", linewidth = 1.5),
+                                 "whiskerprops": dict(color = "#000000", linewidth = 1.5),
+                                 "flierprops":   dict(markeredgecolor = "#ff9900"),
+                                 "meanprops":    dict(markeredgecolor = "#000000")}
 
-                fig.suptitle(f"Histogram and Boxplot of {col}", size = 20, y = 1.02)
-                plt.show()
-        
+                    df.boxplot(by = target, column = [col], widths = 0.5, showmeans = True,
+                               patch_artist = True, vert = False, **box_dict, ax = ax2)
+                    
+                    ax2.set_ylim(ax2.get_ylim()[::-1])
+                    ax2.set_title(None)
+                    ax2.set_xlabel(col)
+                    
+                    boxes = ax2.findobj(matplotlib.artist.Artist)
+                    
+                    if df[target].dtypes == 'O':
+                        for i, box in enumerate(boxes):
+                            if isinstance(box, matplotlib.patches.PathPatch): 
+                                if i < 3: box.set_facecolor("#ea4b33")
+                                if i > 3: box.set_facecolor("#3490d6")
+                    else:
+                        for i, box in enumerate(boxes):
+                            if isinstance(box, matplotlib.patches.PathPatch): 
+                                if i < 3: box.set_facecolor("#3490d6")
+                                if i > 3: box.set_facecolor("#ea4b33")
+                        
+                    fig.suptitle(f"Histogram and Boxplot of {col}", size = 20, y = 1.02)
+                    plt.show()
+                    
+            else:
+                
+                for idx, col in enumerate(num_cols):
+
+                    fig, (ax1, ax2) = plt.subplots(1, 2, figsize = (14, 6))
+
+                    sns.histplot(df, x = df[col], kde = True, hue = target,
+                                 color = sns.color_palette("hls", len(num_cols))[idx], ax = ax1)
+
+                    sns.boxplot(x = df[col], width = 0.4, linewidth = 3, fliersize = 2.5,
+                                color = sns.color_palette("hls", len(num_cols))[idx], ax = ax2)
+
+                    fig.suptitle(f"Histogram and Boxplot of {col}", size = 20, y = 1.02)
+                    plt.show()
+
         # Countplot
         
         if countplot:
@@ -159,35 +238,35 @@ def fast_eda(df, target=None, correlation=True, pairplot=True, hist_box_plot=Tru
                 print(f"{Fore.YELLOW}{Style.BRIGHT}Countplot(s):{Style.RESET_ALL}")
                 print()
 
-                for i in cat_cols:
+                for col in cat_cols:
                     
                     plt.figure(figsize = (12, 8))
 
-                    for j in df[i].value_counts().keys():
-                        if len(str(j)) > 15:
+                    for i in df[col].value_counts().keys():
+                        if len(str(i)) > 15:
                             plt.xticks(fontsize = 8)
                             plt.yticks(fontsize = 8)
                             
-                    large_to_small = df.groupby(i).size().sort_values().index[::-1]        
+                    large_to_small = df.groupby(col).size().sort_values().index[::-1]        
 
-                    if len(df[i].value_counts()) >= 10:
+                    if len(df[col].value_counts()) >= 10:
 
-                        ax = sns.countplot(y = df[i], edgecolor = "#000000", order = large_to_small)
+                        ax = sns.countplot(y = df[col], edgecolor = "#000000", order = large_to_small)
 
                         for container in ax.containers:
                             ax.bar_label(container, padding = 5)
 
-                        plt.title(f"Countplot of {i}", fontsize = 20)      
+                        plt.title(f"Countplot of {col}", fontsize = 20)      
                         plt.show()
 
-                    elif len(df[i].value_counts()) > 1:
+                    elif len(df[col].value_counts()) > 1:
 
-                        ax = sns.countplot(x = df[i], edgecolor = "#000000", order = large_to_small)
+                        ax = sns.countplot(x = df[col], edgecolor = "#000000", order = large_to_small)
 
                         for container in ax.containers:
                             ax.bar_label(container, padding = 5)
 
-                        plt.title(f"Countplot of {i}", fontsize = 20)      
+                        plt.title(f"Countplot of {col}", fontsize = 20)      
                         plt.show()
                 
     else:
